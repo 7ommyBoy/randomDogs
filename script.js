7dd7e49b30c2d74d3e267cf1a31a610443edd672
Tommy Works <93413471+7ommyBoy@users.noreply.github.com>
2022-12-29 15:42:55 -0800

    script.js
    script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('hello')
+// https://dog.ceo/api/breeds/image/random
+// .then - Promises
+// asynchronous programming 
+
+console.log('run 1st')
+
+const dogImageDiv = document.getElementById('dogImage')
+const dogButton = document.getElementById('dogButton')
+
+
+// stuff you have to wait for
+const getNewDog = () => {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(json => {
+      dogImageDiv.innerHTML = `<img src='${json.message}' height=300 width=300/>`
+    })
+}
+
+dogButton.onclick = () => getNewDog()
+console.log('run 3rd')
